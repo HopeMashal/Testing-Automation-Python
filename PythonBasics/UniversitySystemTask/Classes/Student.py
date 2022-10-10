17023782585d01e98689bebbaa75f1de1d3bd5a8
HopeMashal <mashal.hope@gmail.com>
2022-10-10 19:50:00 +0300

    edit student and teacher classes

--- PythonBasics/UniversitySystemTask/Classes/Student.py
+++ PythonBasics/UniversitySystemTask/Classes/Student.py
@@ -5,3 +5,15 @@ class Student:
     self.major = major
     self.birthDate = birthDate
     self.courses = courses
+  
+  def printStudentInfo(self):
+    print(f"Student name is {self.name}. His/Her major is {self.major}. His/Her ID is {self.id}. His/Her birthdate is {self.birthDate}")
+  
+  def printStudentCourse(self):
+    if self.courses == 'none':
+      print(f"This student doesn't have courses yet!!")
+    elif len(self.courses) == 1:
+      print(f"This student's course is {self.courses}")
+    else :
+      print(f"This student's courses are {self.courses}")
+
--- PythonBasics/UniversitySystemTask/Classes/Teacher.py
+++ PythonBasics/UniversitySystemTask/Classes/Teacher.py
@@ -3,3 +3,14 @@ class Teacher:
     self.name = name
     self.id = id
     self.courses = courses
+  
+  def printTeacherInfo(self):
+    print(f"Teacher name is {self.name}. His/Her ID is {self.id}.")
+  
+  def printTeacherCourse(self):
+    if self.courses == 'none':
+      print(f"This teacher doesn't have courses yet!!")
+    elif len(self.courses) == 1:
+      print(f"This teacher's course is {self.courses}")
+    else :
+      print(f"This teacher's courses are {self.courses}")
