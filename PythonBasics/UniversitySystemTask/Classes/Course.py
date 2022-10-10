d234ac899ac42d07c481dbec72796f065f44ffbe
HopeMashal <mashal.hope@gmail.com>
2022-10-10 20:01:07 +0300

    edit courses

--- PythonBasics/UniversitySystemTask/Classes/Course.py
+++ PythonBasics/UniversitySystemTask/Classes/Course.py
@@ -1,6 +1,23 @@
 class Course:
-  def __init__(self,name,numOfHours,teacher,students='none'):
+  teacher = 'none'
+  students = []
+
+  def __init__(self,name,numOfHours,teacher,students):
     self.name = name
     self.numOfHours = numOfHours
     self.teacher = teacher
-    self.students = students
+    self.students.extend(students)
+  
+  def printCourseInfo(self):
+    print(f"Course name is {self.name}. Its number of hours is {self.numOfHours}.")
+  
+  def printStudentsList(self):
+    if self.students == 'none':
+      print(f"This course doesn't have students yet!!")
+    elif len(self.students) == 1:
+      print(f"The student of this course is {self.students}")
+    else :
+      print(f"The students of this course are {self.students}")
+  
+  def printTeacher(self):
+    print(f"The teacher of this course is {self.teacher}")
